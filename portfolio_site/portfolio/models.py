8e1a17ebcd1d33a0fae5f71b51b804e76e38fc64
Vadym <vadymkravtcsov@gmail.com>
2025-12-11 00:04:24 +0200

    Add PortfolioModels class with fields for project details

--- portfolio_site/portfolio/models.py
+++ portfolio_site/portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class PortfolioModels(models.Model):
+    title = models.CharField("Projects", max_length=50)
+    description = models.CharField("Descriptions", max_length=255)
+    image = models.ImageField(
+        "Images",
+        upload_to="images/",
+        height_field="height_field",
+        width_field="width_field")
+    height_field = models.PositiveIntegerField() 
+    width_field = models.PositiveIntegerField()
+    links = models.URLField("Git Links", max_length=255)
+    technologies = models.CharFields("Technogies", max_length=255)
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+        
