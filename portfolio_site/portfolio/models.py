95b01b4482ff9876bf61fb4cbd409bf2aac99ff3
Vadym <vadymkravtcsov@gmail.com>
2025-12-21 23:55:55 +0200

    feat: simplify Portfolio model by removing unused fields for image dimensions

--- portfolio_site/portfolio/models.py
+++ portfolio_site/portfolio/models.py
@@ -4,13 +4,10 @@ from django.db import models
 class Portfolio(models.Model):
     title = models.CharField("Projects", max_length=50, unique=True)
     description = models.CharField("Descriptions", max_length=255)
+
     image = models.ImageField(
         "Images",
-        upload_to="images/",
-        height_field="height_field",
-        width_field="width_field")
-    height_field = models.PositiveIntegerField() 
-    width_field = models.PositiveIntegerField()
+        upload_to="images/")
     links = models.URLField("Git Links", max_length=255)
     technologies = models.CharField("Technogies", max_length=255)
 
