b400dc47abb31da440479d04e5bedcb849c47be3
Vadym <vadymkravtcsov@gmail.com>
2026-01-04 23:45:19 +0200

    fix: a view is createdthat loadsall related models with the first query to the bd.

--- portfolio_site/projects/views.py
+++ portfolio_site/projects/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from projects.models import Projects
+from projects.models import Project
+
 
 
 def index(request):
-    projects = Projects.objects.all()
-    context = {
-        "projects": projects,
-        "title": "Vadym Kravtsov",
-        }
-    return render(request, "projects/index.html",context=context)
+    project = Project.objects.select_related(
+        'profile__author'
+    ).prefetch_related(
+        'profile__skills__icon'
+    ).first()
+    title = project.profile.author
+    return render(request, 'projects/index.html', {project: 'projects', 'title': f'Protfolio {title}'})
