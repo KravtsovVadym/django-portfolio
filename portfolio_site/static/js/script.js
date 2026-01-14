eff396366dbbb4e4141fb4761a49500e113a88be
Vadym <vadymkravtcsov@gmail.com>
2026-01-14 23:17:30 +0200

    feat: addedfunction java for theme switcher

--- portfolio_site/static/js/script.js
+++ portfolio_site/static/js/script.js
@@ -6,3 +6,27 @@ window.addEventListener('scroll', function() {
     divider.classList.remove('visible');
   }
 });
+
+
+// theme switcher
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+const icon = themeToggle.querySelector('i');
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'light') {
+  body.classList.add('light-theme');
+  icon.classList.replace('bi-sun-fill', 'bi-moon-fill');
+}
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('light-theme');
+  
+  if (body.classList.contains('light-theme')) {
+    icon.classList.replace('bi-sun-fill', 'bi-moon-fill');
+    localStorage.setItem('theme', 'light');
+  } else {
+    icon.classList.replace('bi-moon-fill', 'bi-sun-fill');
+    localStorage.setItem('theme', 'dark');
+  }
+});
