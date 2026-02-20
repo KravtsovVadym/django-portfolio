f8fc0014f6374339b91756355a2b881f696ffbbe
Vadym <vadymkravtcsov@gmail.com>
2026-02-20 02:20:34 +0200

    style: update comments script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,43 @@
+// Page scroll event handler
 window.addEventListener('scroll', function() {
   const divider = document.querySelector('.custom-divider-nav');
+  // Check if scrolled more than 80 pixels
   if (window.pageYOffset > 80) {
     divider.classList.add('visible');
   } else {
+  // Hide the separator
     divider.classList.remove('visible');
   }
 });
 
 
-// theme switcher
+// Theme switcher
+// We get elements for working with the theme
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
 const icon = themeToggle.querySelector('i');
 
+// Check the topic stored in localStorage
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'light') {
+// Apply a light theme when loading
   body.classList.add('light-theme');
   icon.classList.replace('bi-sun-fill', 'bi-moon-fill');
 }
 
+// Theme switch click handler
 themeToggle.addEventListener('click', () => {
   body.classList.toggle('light-theme');
   
   if (body.classList.contains('light-theme')) {
+// Change the icon to the moon for the light theme
     icon.classList.replace('bi-sun-fill', 'bi-moon-fill');
+// Save the light theme selection
     localStorage.setItem('theme', 'light');
   } else {
+// Change the icon to the sun for the dark theme
     icon.classList.replace('bi-moon-fill', 'bi-sun-fill');
+// Save the dark theme selection
     localStorage.setItem('theme', 'dark');
   }
 });
